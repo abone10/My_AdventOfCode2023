b97bc2ecd34a6353899c52c8ebe481e2794ba1b1
abone10 <ale.bonechi2005@gmail.com>
2024-01-29 00:14:57 +0100

    Day 5 near end ATTENTION: may crash your pc

--- Day5/IfYouGiveASeedAFertilizer.py
+++ Day5/IfYouGiveASeedAFertilizer.py
@@ -8,9 +8,41 @@ file.close()
 data = "".join(data)
 data = p1.split(data)
 
+print(' check 1')
 for i, line in enumerate(data):
     data[i] = re.split(r'\n', line)
+    for j, set in enumerate(data[i]):
+        data[i][j] = re.split(r'\s',set)
 
-print(data)
+semi = data[1][0]
 data.pop(0)
-print(data)
+data.pop(0)
+
+print(' check 2')
+
+mappe = []
+for i, group in enumerate(data):
+    mappe.append({})
+    print(' check 2')
+    for set in group:
+        print('check 3')
+        for n in range(int(set[2])):
+            mappe[i][str(int(int(set[1]))+n)] = str(int(set[0])+n)
+
+print('check 3')
+print(mappe[0])
+result = 9999999999999999
+for i, seme in enumerate(semi):
+    location=semi[i]
+    for mappa in mappe:
+        lista = mappa.keys()
+        if location in lista:
+            location = mappa[location]
+
+
+    if int(location) < result:
+        result = int(location)
+
+
+
+print(result)
