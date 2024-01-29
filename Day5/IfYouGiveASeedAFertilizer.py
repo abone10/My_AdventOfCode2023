aff1d4f40de875401b7dd6ef97099df619a8b618
abone10 <ale.bonechi2005@gmail.com>
2024-01-29 09:21:07 +0100

    day 5 part 1 finished

--- Day5/IfYouGiveASeedAFertilizer.py
+++ Day5/IfYouGiveASeedAFertilizer.py
@@ -14,35 +14,28 @@ for i, line in enumerate(data):
     for j, set in enumerate(data[i]):
         data[i][j] = re.split(r'\s',set)
 
+
 semi = data[1][0]
 data.pop(0)
 data.pop(0)
+for i in range(len(semi)):
+    semi[i] = int(semi[i])
+
+
+for i in range(len(data)):
+    print('check 1.5')
+    for j in range(len(data[i])):
+        for z in range(len(data[i][j])):
+            data[i][j][z] = int(data[i][j][z])
 
 print(' check 2')
 
-mappe = []
 for i, group in enumerate(data):
-    mappe.append({})
     print(' check 2')
     for set in group:
         print('check 3')
-        for n in range(int(set[2])):
-            mappe[i][str(int(int(set[1]))+n)] = str(int(set[0])+n)
+        for i in range(len(semi)):
+            if(set[1]<semi[i]<set[1]+set[2]):
+                semi[i]=set[0]+semi[i]-set[1]
 
-print('check 3')
-print(mappe[0])
-result = 9999999999999999
-for i, seme in enumerate(semi):
-    location=semi[i]
-    for mappa in mappe:
-        lista = mappa.keys()
-        if location in lista:
-            location = mappa[location]
-
-
-    if int(location) < result:
-        result = int(location)
-
-
-
-print(result)
+print(min(semi))
